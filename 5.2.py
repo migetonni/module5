88fe218e2724670fb7690595bdcfcbcb1e91c002
migetonni <Mixu.Koski-Homi@metropolia.fi>
2022-09-05 15:01:07 +0300

    5.2

---
+++ 5.2.py
@@ -0,0 +1,18 @@
+numbers = []
+readingNumbers = True
+while readingNumbers:
+    strInput = input("Anna luku: ")
+    if strInput == "":
+        readingNumbers = False
+    else:
+        numbers.append(int(strInput))
+
+print(numbers)
+
+numbers.sort(reverse=True)
+print(numbers)
+print(numbers[0:5])
+for number in numbers[:5]:
+    print(number)
+
+
